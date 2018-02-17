490686d96a1e5bd2c9fb28537c68089acc889594
arun <arun.kirubarajan@gmail.com>
2018-02-16 20:02:07 -0500

    Add userdata to member schema

--- models/member.js
+++ models/member.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 
 const MemberSchema = new mongoose.Schema({
   name: String,
+  username: String,
   bio: String,
+  major: String,
   location: String,
+  photo: String,
+  role: String,
+  current: Boolean,  
   linkedin: String,
   website: String,
   github: String
