b549b72f7dee4ba31e8b07435a1804ad25ab407d
Arun Kirubarajan <arun@huntsman-ve505-0661.apn.wlan.upenn.edu>
2018-08-01 13:49:13 -0400

    Useless comments are useless

--- index.js
+++ index.js
@@ -1,72 +1,25 @@
-// Import frameworks
 const express = require('express');
 const handlebars = require('express-handlebars');
 const body_parser = require('body-parser');
-const mongoose = require('mongoose');
 const path = require('path');
 const models = require('./models');
 const app = express();
 
-// Configure the app environment
 app.use(body_parser.urlencoded({ extended: true }));
 app.use(body_parser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.set('view engine', 'handlebars');
+app.use('/static', express.static(path.join(__dirname, 'views/static')));
 app.engine('handlebars', handlebars({
   defaultLayout: 'main',
 }));
-app.set('view engine', 'handlebars');
-app.use('/static', express.static(path.join(__dirname, 'views/static')));
 
 models.connect(process.env.DB_URI);
 
-// Handle routing
-app.get('/', (req, res) => {
-  res.render('splash');
-});
+const pageRoutes = require('./routes/pages');
+app.use('/', pageRoutes);
 
-app.get('/team/:username', (req, res) => {
-  mongoose.model('Member').findOne({username: req.params.username}, (err, member) => {
-    res.render('member', member);
-  });
-});
-
-app.get('/team', (req, res) => {
-  mongoose.model('Member').find({}, (err, members) => {
-    res.render('team', { members });
-  });
-});
-
-app.get('/contact', (req, res) => {
-  res.render('contact');
-});
-
-app.get('/docs', (req, res) => {
-  res.render('docs');
-});
-
-app.get('/products', (req, res) => {
-  res.render('products');
-});
-
-app.get('/docs/api', (req, res) => {
-  res.render('docs_page');
-});
-
-app.get('/mobile', (req, res) => {
-  res.redirect('products');
-});
-
-app.get('/mobile/*', (req, res) => {
-  res.redirect('../products');
-});
-
-// Catch-all route for 404 errors
-app.get('*', (req, res) => {
-  res.render('error');
-});
-
-// Start the app
 app.listen(process.env.PORT, err => {
   if (err) {
     console.error(err);
---
+++ routes/pages.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = new express.Router();
+
+app.get('/', (req, res) => {
+  res.render('splash');
+});
+
+app.get('/team/:username', (req, res) => {
+  mongoose.model('Member').findOne({ username: req.params.username }, (err, member) => {
+    res.render('member', member);
+  });
+});
+
+app.get('/team', (req, res) => {
+  mongoose.model('Member').find({}, (err, members) => {
+    res.render('team', { members });
+  });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/docs', (req, res) => {
+  res.render('docs');
+});
+
+app.get('/products', (req, res) => {
+  res.render('products');
+});
+
+app.get('/docs/api', (req, res) => {
+  res.render('docs_page');
+});
+
+app.get('/mobile', (req, res) => {
+  res.redirect('products');
+});
+
+app.get('/mobile/*', (req, res) => {
+  res.redirect('../products');
+});
+
+// Catch-all route for 404 errors
+app.get('*', (req, res) => {
+  res.render('error');
+});
+
+module.exports = app;
