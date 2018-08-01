bee9533aba7cb282a7b5ad3a4a187144ee69a0ab
Arun Kirubarajan <arun@huntsman-ve505-0661.apn.wlan.upenn.edu>
2018-08-01 14:23:21 -0400

    Add product page markdown

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/basics', (req, res) => {
-  res.render('api');
+  res.render('product', { 
+      'name': 'PennBasics', 
+      'description': "PennBasics is a one-stop shop for the basics of living at Penn. It strives to make students' lives easier at Penn so they can focus on what matters. PennBasics aggregates essential data on dining, laundry, and study spaces, and tailors the dashboard to your preferences.",
+      'url': "https://pennbasics.com",
+      'github': "https://github.com/pennlabs/pennbasics"
+  });
 });
 
 module.exports = app;
