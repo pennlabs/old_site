0fef2ff76cc224b476b0891cc3381b8eea740af7
Arun Kirubarajan <arun@huntsman-ve507-0259.apn.wlan.upenn.edu>
2018-08-06 11:16:46 -0400

    Add markdown parsing

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const showdown  = require('showdown');
 const app = new express.Router();
 
 app.get('/', (req, res) => {
@@ -7,11 +8,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/basics', (req, res) => {
-  res.render('product', { 
+  mongoose.model('Documentation').findOne({ product: "basics" }, (err, doc) => {
+    const converter = new showdown.Converter();
+    doc.body = converter.makeHtml(doc.body);
+    
+    res.render('product', { 
       'name': 'PennBasics', 
       'description': "PennBasics is a one-stop shop for the basics of living at Penn. It strives to make students' lives easier at Penn so they can focus on what matters. PennBasics aggregates essential data on dining, laundry, and study spaces, and tailors the dashboard to your preferences.",
       'url': "https://pennbasics.com",
-      'github': "https://github.com/pennlabs/pennbasics"
+      'github': "https://github.com/pennlabs/pennbasics",
+      doc
+    });
   });
 });
 
